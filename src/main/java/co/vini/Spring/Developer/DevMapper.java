9c402651f4f76933de5abcd21d55355686753bd3
Vincius <viniviniee11@gmail.com>
2025-12-29 14:54:12 -0300

    feat: adds map method to deal with DTO

--- src/main/java/co/vini/Spring/Developer/DevMapper.java
+++ src/main/java/co/vini/Spring/Developer/DevMapper.java
@@ -1,4 +1,39 @@
 package co.vini.Spring.Developer;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.Mapping;
+
+@Component
 public class DevMapper {
+
+    public DevModel map(DevDTO devDTO){
+
+        DevModel devModel = new DevModel();
+
+        devModel.setId(devDTO.getId());
+        devModel.setName(devDTO.getName());
+        devModel.setEmail(devDTO.getEmail());
+        devModel.setRole(devDTO.getRole());
+        devModel.setPfpUrl(devDTO.getPfpUrl());
+        devModel.setRank(devDTO.getRank());
+        devModel.setTasks(devDTO.getTasks());
+
+        return devModel;
+    }
+
+    public DevDTO map(DevModel devModel){
+
+        DevDTO devDTO = new DevDTO();
+
+        devDTO.setId(devModel.getId());
+        devDTO.setName(devModel.getName());
+        devDTO.setEmail(devModel.getEmail());
+        devDTO.setRole(devModel.getRole());
+        devDTO.setPfpUrl(devModel.getPfpUrl());
+        devDTO.setRank(devModel.getRank());
+        devDTO.setTasks(devModel.getTasks());
+
+        return devDTO;
+
+    }
 }
