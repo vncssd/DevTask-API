aac05690aa1c86d0a32d843bf17d374a9aaaad1f
Vincius <viniviniee11@gmail.com>
2025-12-29 14:54:42 -0300

    feat: changes devModel responsibility to devDTO

--- src/main/java/co/vini/Spring/Developer/DevService.java
+++ src/main/java/co/vini/Spring/Developer/DevService.java
@@ -1,43 +1,54 @@
 package co.vini.Spring.Developer;
 
+import org.hibernate.query.sqm.mutation.internal.temptable.UpdateExecutionDelegate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class DevService {
 
     private DevRepository devRepository;
+    private DevMapper devMapper;
 
-    public DevService(DevRepository devRepository) {
+    public DevService(DevRepository devRepository, DevMapper devMapper) {
         this.devRepository = devRepository;
+        this.devMapper = devMapper;
     }
 
     //list all devs
-    public List<DevModel> listDevs(){
-        return devRepository.findAll();
+    public List<DevDTO> listDevs(){
+        List<DevModel> devs = devRepository.findAll();
+        return devs.stream()
+                .map(devMapper::map)
+                .collect(Collectors.toList());
     }
 
     //list by id
-    public DevModel listDevsById(Long id){
+    public DevDTO listDevsById(Long id){
         Optional<DevModel> devModelOptional = devRepository.findById(id);
-        return devModelOptional.orElse(null);
+        return devModelOptional.map(devMapper::map).orElse(null);
     }
 
-    public DevModel createDev(DevModel devModel){
-        return devRepository.save(devModel);
+    public DevDTO createDev(DevDTO devDTO){
+        DevModel dev = devMapper.map(devDTO);
+        return devMapper.map(devRepository.save(dev));
     }
 
     public void deleteDev(Long id){
         devRepository.deleteById(id);
     }
 
-    public DevModel updateDev(Long id, DevModel devModelUpdated){
-        if (devRepository.existsById(id)){
-            devModelUpdated.setId(id);
-            return devRepository.save(devModelUpdated);
+    public DevDTO updateDev(Long id, DevDTO devDTO){
+        Optional<DevModel> devModelOptional = devRepository.findById(id);
+        if (devModelOptional.isPresent()){
+            DevModel updatedDev = devMapper.map(devDTO);
+            updatedDev.setId(id);
+            DevModel savedDev = devRepository.save(updatedDev);
+            return devMapper.map(savedDev);
         }
         return null;
     }
