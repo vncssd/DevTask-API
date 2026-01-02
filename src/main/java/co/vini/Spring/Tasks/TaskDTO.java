a9db91b020004718f55f596a615e908abb6a05b1
Vincius <viniviniee11@gmail.com>
2026-01-02 16:33:39 -0300

    feat: creates DTO to add a abstraction layer in TaskModel

--- src/main/java/co/vini/Spring/Tasks/TaskDTO.java
+++ src/main/java/co/vini/Spring/Tasks/TaskDTO.java
@@ -1,4 +1,20 @@
 package co.vini.Spring.Tasks;
 
-public class TaskDTo {
+import co.vini.Spring.Developer.DevModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class TaskDTO {
+
+    private Long id;
+    private String name;
+    private String level;
+    private List<DevModel> developer;
+
 }
