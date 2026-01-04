8a97035e5d744e213392f6827badb4d3b5962548
Vincius <viniviniee11@gmail.com>
2026-01-04 15:22:14 -0300

    refactor: adds ResponseEntity to request response on controller layer

--- src/main/java/co/vini/Spring/Tasks/TaskController.java
+++ src/main/java/co/vini/Spring/Tasks/TaskController.java
@@ -1,4 +1,9 @@
 package co.vini.Spring.Tasks;
+import com.sun.net.httpserver.HttpsServer;
+import org.apache.coyote.Response;
+import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,18 +20,38 @@ public class TaskController {
     }
 
     @PostMapping("/add")
-    public TaskDTO createTask(@RequestBody TaskDTO task){
-        return taskService.createTask(task);
+    public ResponseEntity<String> createTask(@RequestBody TaskDTO task){
+        TaskDTO createdTask = taskService.createTask(task);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("TASK" + createdTask.getName() + " ADDED SUCCESFULLY ID: " +createdTask.getId());
     }
 
     @PutMapping("/edit")
-    public String editTask(){
-        return "task edited successfully";
+    public ResponseEntity<String> update (@PathVariable Long id, @RequestBody TaskDTO taskUpdated){
+        TaskDTO task = taskService.updateTask(id, taskUpdated);
+
+        if (task != null){
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body("TASK WITH ID " + task.getId() + " UPDATED");
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("TASK WITH ID " + task.getId() + " NOT FOUND");
+        }
     }
 
     @DeleteMapping("/delete/{id}")
-    public void deleteTask(@PathVariable Long id){
+    public ResponseEntity<String> deleteTask(@PathVariable Long id){
         taskService.deleteTask(id);
+
+        if (taskService.listTaskById(id) != null){
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body("TASK WITH ID " + id + " DELETED");
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("TASK WITH ID " + id + " NOT FOUND");
+        }
     }
 
     @GetMapping("/list")
@@ -35,7 +60,15 @@ public class TaskController {
     }
 
     @GetMapping("/list/{id}")
-    public TaskDTO listTaskById(@PathVariable Long id){
-        return taskService.listTaskById(id);
+    public ResponseEntity<?> listTaskById(@PathVariable Long id){
+        TaskDTO task = taskService.listTaskById(id);
+        if (task != null){
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(task);
+        }
+        else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("TASK WITH ID " + id + " NOT FOUND");
+        }
     }
 }
